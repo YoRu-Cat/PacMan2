2ecd69b2c14abfb3cf923d2a5c0f73bf386c4b66
YoRu Tech <tech.niko.coding@gmail.com>
2024-03-12 12:28:07 +0500

    make pacman move within boundary

--- app.js
+++ app.js
@@ -134,19 +134,50 @@ document.addEventListener("DOMContentLoaded", () => {
     switch (e.keyCode) {
       //left
       case 37:
-        pacmanCurrentIndex -= 1;
+        if (
+          pacmanCurrentIndex % width !== 0 &&
+          !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
+          !squares[pacmanCurrentIndex - 1].classList.contains("ghost-lair")
+        ) {
+          pacmanCurrentIndex -= 1;
+        }
+        if (squares[pacmanCurrentIndex - 1] === squares[363]) {
+          pacmanCurrentIndex = 391;
+        }
         break;
       //up
       case 38:
-        pacmanCurrentIndex -= width;
+        if (
+          pacmanCurrentIndex - width >= 0 &&
+          !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
+          !squares[pacmanCurrentIndex - width].classList.contains("ghost-lair")
+        ) {
+          pacmanCurrentIndex -= width;
+        }
         break;
       //right
       case 39:
-        pacmanCurrentIndex += 1;
+        if (
+          pacmanCurrentIndex % width < width - 1 &&
+          !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
+          !squares[pacmanCurrentIndex + 1].classList.contains("ghost-lair")
+        ) {
+          pacmanCurrentIndex += 1;
+        }
+        if (squares[pacmanCurrentIndex + 1] === squares[392]) {
+          pacmanCurrentIndex = 364;
+        }
+
         break;
       //down
       case 40:
-        pacmanCurrentIndex += width;
+        if (
+          pacmanCurrentIndex + width < width * width &&
+          !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
+          !squares[pacmanCurrentIndex + width].classList.contains("ghost-lair")
+        ) {
+          pacmanCurrentIndex += width;
+        }
         break;
     }
     squares[pacmanCurrentIndex].classList.add("pac-man");
